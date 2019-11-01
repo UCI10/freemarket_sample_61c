7235306a835a50b3b5904dacaf84a23c3ee99fc6
saitou-daiki <alsocorykk@gmail.com>
2019-11-01 20:25:14 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   root to: 'tops#index'
   resources :products
   resources :users 
+
+  resources :signup do
+    collection do
+      get 'step0'
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
 end
