6281579279f9c34a3261b7ba07db946036d10113
saitou-daiki <alsocorykk@gmail.com>
2019-11-05 19:15:51 +0900

    プログレスバー修正　サーバーサイド３まで実施

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -33,23 +33,52 @@ def step3
   # session[:last_name_kana] = user_params[:last_name_kana]
   # session[:first_name_kana] = user_params[:first_name_kana]
 
-  session[:phone_number] = session =[:phone_number]
+  session[:phone_number] = user_params[:phone_number]
 
   @user = User.new # 新規インスタンス作成
+  @address = Address.new
 
 end
 
 def step4
 
-  session[:last_name] = user_params[:last_name]
-  session[:first_name] = user_params[:first_name]
-  session[:last_name_phonetic] = user_params[:last_name_phonetic]
-  session[:first_name_phonetic] = user_params[:first_name_phonetic]
+  # session[:last_name] = user_params[:last_name]
+  # session[:first_name] = user_params[:first_name]
+  # session[:last_name_phonetic] = user_params[:last_name_phonetic]
+  # session[:first_name_phonetic] = user_params[:first_name_phonetic]
 
-  
+  session[:postalcode] = address_params[:postalcode]
+  session[:city] = address_params[:city]
+  session[:house_number] = address_params[:house_number]
+  session[:building_name  ] = address_params[:building_name]
+  session[:prefecture] = address_params[:prefecture]
 
+  # @user = User.new(
+  #   nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+  #   email: session[:email],
+  #   password: session[:password],
+  #   password_confirmation: session[:password_confirmation],
+  #   last_name: session[:last_name], 
+  #   first_name: session[:first_name], 
+  #   last_name_kana: session[:last_name_kana], 
+  #   first_name_kana: session[:first_name_kana],
+  #   birth_year: session[:birth_year],
+  #   birth_month: session[:birth_month],
+  #   birth_day: session[:birth_day],
+  #   phone_number: session[:phone_number]
 
-@user = User.new
+  # )
+#   if @user.save
+# # ログインするための情報を保管
+#     session[:id] = @user.id
+#     redirect_to done_signup_index_path
+#   else
+#     render '/signup/entry_signup'
+#   end
+
+  #カードのアソシエーションを組むこと。
+# @user = User.new
+
 end
 
 
@@ -62,7 +91,11 @@ def create
     last_name: session[:last_name], 
     first_name: session[:first_name], 
     last_name_kana: session[:last_name_kana], 
-    first_name_kana: session[:first_name_kana]
+    first_name_kana: session[:first_name_kana],
+    birth_year: session[:birth_year],
+    birth_month: session[:birth_month],
+    birth_day: session[:birth_day],
+    phone_number: session[:phone_number]
 
   )
   if @user.save
@@ -70,7 +103,7 @@ def create
     session[:id] = @user.id
     redirect_to done_signup_index_path
   else
-    render '/signup/registration'
+    render '/signup/entry_signup'
   end
 end
 
@@ -87,16 +120,38 @@ end
       :email, 
       :password, 
       :password_confirmation, 
+
       :last_name, 
       :first_name, 
       :last_name_phonetic, 
       :first_name_phonetic,
 
+      :phone_number,
 
       :birth_year,
       :birth_month,
       :birth_day
+
+ 
+ 
     )
   end
+
+  def address_params
+    params.require(:address).permit(
+      :prefectures,
+      :postalcode,
+      :city,
+      :house_number,
+      :building_name
+    )
+
+  end
+
+
+
+
+  
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresss
+  has_one :address
 end
