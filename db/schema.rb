72587121f59c3e7d35e44e9a0ba6244c714b8322
takahata-noria <takahatanoria@gmail.com>
2019-11-06 15:55:37 +0900

    カテゴリ系変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_060400) do
+ActiveRecord::Schema.define(version: 2019_11_05_111008) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
