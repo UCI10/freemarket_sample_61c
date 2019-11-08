f41a02302fdd32c5de636eaef50b282d298550cc
saitou-daiki <alsocorykk@gmail.com>
2019-11-09 02:10:13 +0900

    バリデーション完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
 
 
-
-
-  # validates :email, {presence: true}
-
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # validates :email, presence: true,  format: { with: VALID_EMAIL_REGEX }
 
@@ -23,23 +19,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :address
-  has_one :cards
+  has_one :card
 
   VALID_EMAIL_REGEX =                 /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  validates :email,                 presence: true, length:{maximum: 255}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}, on: :validates_step1
+  validates :nickname,              presence: true, length: {maximum: 20}, on: :validates_step1
+  # validates :email,                 presence: true, length:{maximum: 255}, uniqueness: true, on: :validates_step1
+  validates :password,              presence: true, length: {minimum: 7, maxinum: 128}, on: :validates_step1
+  validates :password_confirmation, presence: true, length: {minimum: 7, maxinum: 128}, on: :validates_step1
+  validates :last_name,             presence: true, on: :validates_step1
+  validates :first_name,            presence: true, on: :validates_step1
+  validates :last_name_phonetic,    presence: true, on: :validates_step1
+  validates :first_name_phonetic,   presence: true, on: :validates_step1
+  validates :birth_year,            presence: true, on: :validates_step1
+  validates :birth_month,           presence: true, on: :validates_step1 
+  validates :birth_day,             presence: true, on: :validates_step1
+  
+  validates :phone_number,          presence: true, on: :validates_step2
 
-  # step1入力項目
-  validates :nickname,              presence: true, length: {maximum: 20}
-  validates :email,                 presence: true, length:{maximum: 255}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
-  validates :password,              presence: true, length: {minimum: 7, maxinum: 128}
-  validates :password_confirmation, presence: true, length: {minimum: 7, maxinum: 128}
-  validates :last_name,             presence: true
-  validates :first_name,            presence: true 
-  validates :last_name_phonetic,    presence: true 
-  validates :first_name_phonetic,   presence: true 
-  validates :birth_year,            presence: true 
-  validates :birth_month,           presence: true 
-  validates :birth_day,             presence: true 
 
   # def email_custom_error
   #   if email.blank?
