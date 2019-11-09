a2744a76723d850063db89cf92cf060b5c255793
takahata-noria <takahatanoria@gmail.com>
2019-11-09 13:46:00 +0900

    コメントアウト削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   before_action :sell, only: [:new, :edit]
-  # before_action :sell, only: [:new, :edit, :update]
 
   def index
 
@@ -16,29 +15,12 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     
     if @product.save
-      # respond_to do |format|
-      #   format.json
-      # end
+
     else
       @product.images.build 
       render action: :new
     end
-    # Product.create(title: product_params[:title], description: product_params[:description], user_id: current_user.id)
-    
-    # @product = Product.create(product_params)
-    # @product = Product.new(product_params)
-    # @product.images.build   
-    # image = @product.images.new(image_url: image_url)
-    # image.save
 
-    # image_url = @product.images.new(image_url: image_url)
-    # image_url.save
-
-    # if @product.save
-    #   redirect_to root_path
-    # else
-    #   redirect_to action: :new
-    # end
   end
   
   def show
@@ -69,8 +51,7 @@ private
 
   def sell
     @category = Category.where(ancestry: nil) 
-    # @children = Category.none
-    # @grandchildren = Category.none
+
   end 
 
 end
