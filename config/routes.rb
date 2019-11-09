676c13a49f429f307fc2aa09d38b9c81efe567d1
saitou-daiki <alsocorykk@gmail.com>
2019-11-09 14:03:29 +0900

    pay.jp途中まで。

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tops#index'
-  resources :products
+  resources :products do
+    collection do
+      post 'purchase'
+    end
+  end
+
   resources :users 
 
   resources :signup do
