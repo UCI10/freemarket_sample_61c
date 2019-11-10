9f2a7ce4e0a6a1a41a8502ea8048efe37a2bf2a4
saitou-daiki <alsocorykk@gmail.com>
2019-11-10 15:48:51 +0900

    usersにprovideとuidのカラムを追加。フェイスブックとグーグル認証実装のため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_060233) do
+ActiveRecord::Schema.define(version: 2019_11_10_064527) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -109,6 +109,8 @@ ActiveRecord::Schema.define(version: 2019_11_07_060233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
