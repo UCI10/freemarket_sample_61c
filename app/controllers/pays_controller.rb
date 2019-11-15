67c9c989b4f4afc6fd413f032d159340dcdac637
saitou-daiki <alsocorykk@gmail.com>
2019-11-15 13:24:25 +0900

    card_idをstring型に変更

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -2,6 +2,18 @@ class PaysController < ApplicationController
 
   require 'payjp'
 
+  def new
+    pay = current_user.credit_card
+    if pay.blank?
+      redirect_to controller: 'products', action: "show" 
+    else
+      Payjp.api_key = "sk_test_0e2eb234eabf724bfaa4e676"
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @customer_card = customer.cards.retrieve(card.card_id)
+    end
+    
+  end
+
   def pay
     Payjp.api_key = 'sk_test_0ddb364bab7ed621b29956cb'
     charge = Payjp::Charge.create(
@@ -11,35 +23,47 @@ class PaysController < ApplicationController
   )
   end
 
-  # def index
-  #   card = Card.where(user_id: current_user.id).first
-  #   #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
-  #   if card.blank?
-  #     #cardテーブルは前回記事で作成、テーブルからpay.jpの顧客IDを検索
-  #   if card.blank?
-  #     #登録された情報がない場合にカード登録画面に移動
-  #     redirect_to controller: "card", action: "new"
-  #   else
-  #     payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-  #     #保管した顧客IDでpay.jpから情報取得
-  #     customer = Payjp::Customer.retrieve(card.customer_id)
-  #     #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-  #     @default_card_information = customer.cards.retrieve(card.card_id)
-  #   end
-  #   end
+  def buy #クレジット購入
 
-    def pay
-      card = Card.where(user_id: current_user.id).first
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    if pay.blank?
+      redirect_to action: "new"
+      flash[:alert] = '購入にはクレジットカード登録が必要です'
+    else
+      @product = Product.find(params[:product_id])
+     # 購入した際の情報を元に引っ張ってくる
+      pay = current_user.credit_card
+     # テーブル紐付けてるのでログインユーザーのクレジットカードを引っ張ってくる
+      Payjp.api_key = "sk_test_a0947663b395402fc1e150b4"
+     # キーをセットする(環境変数においても良い)
       Payjp::Charge.create(
-      :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
-      :customer => card.customer_id, #顧客ID
-      :currency => 'jpy', #日本円
-    )
-    redirect_to action: 'done' #完了画面に移動
+      amount: @product.price, #支払金額
+      customer: pay.customer_id, #顧客ID
+      currency: 'jpy', #日本円
+      )
+     # ↑商品の金額をamountへ、cardの顧客idをcustomerへ、currencyをjpyへ入れる
+      if @product.update(status: 1, buyer_id: current_user.id)
+        flash[:notice] = '購入しました。'
+        redirect_to controller: "products", action: 'show'
+      else
+        flash[:alert] = '購入に失敗しました。'
+        redirect_to controller: "products", action: 'show'
+      end
+     #↑この辺はこちら側のテーブル設計どうりに色々しています
     end
+  end
 
-    
-
+  def delete
+    pay = current_user.credit_card
+    if pay.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = 'sk_test_0e2eb234eabf724bfaa4e676'
+      customer = Payjp::Customer.retrieve(pay.customer_id)
+      customer.delete
+     #ここでpay.jpの方を消している
+      card.delete
+     #ここでテーブルにあるcardデータを消している
+    end  
+end
 
 end
